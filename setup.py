8fb56d268cd5cc8ad985e1d9fde7c44e9c0cad02
ramusus <ramusus@gmail.com>
2013-03-23 15:38:12 +0530

    version 0.1.4, without requirements.text

--- setup.py
+++ setup.py
@@ -14,8 +14,16 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
-    dependency_links=[re.sub(r'^.+\+', '', r.strip()) for r in open('requirements.txt').readlines() if 'egg=' in r],
-    install_requires=[re.sub(r'^.+egg=', '', r.strip()) for r in open('requirements.txt').readlines()],
+    dependency_links = [
+        'https://github.com/iplatform/pyFaceGraph/tarball/master#egg=pyfacegraph-0.0.36',
+    ],
+    install_requires=[
+        'django',
+        'django-annoying',
+        'django-oauth-tokens>=0.2.2',
+        'pyfacegraph>=0.0.36',
+        'python-dateutil>=1.5',
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
