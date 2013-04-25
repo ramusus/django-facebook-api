ee9352433ccb41aa08c71cdf02f710355c62fa0e
ramusus <ramusus@gmail.com>
2013-04-26 02:39:23 +0400

    added handling error response

--- facebook_api/tests.py
+++ facebook_api/tests.py
@@ -11,3 +11,9 @@ class FacebookApiTest(TestCase):
         self.assertEqual(response.last_name, 'Zuckerberg')
         self.assertEqual(response.first_name, 'Mark')
         self.assertEqual(response.gender, 'male')
+
+    def test_empty_result(self):
+
+        result = graph('8576093908/posts', **{'limit': 1000, 'until': 1345661805, 'offset': 0})
+        if result is not None:
+            self.assertEqual(result.error_code, 1)
--- facebook_api/utils.py
+++ facebook_api/utils.py
@@ -1,11 +1,14 @@
+from django.conf import settings
 from oauth_tokens.models import AccessToken
 from facegraph import Graph, GraphException
 from datetime import datetime
 import logging
 
-__all__ = ['api_call']
+__all__ = ['graph']
 
 log = logging.getLogger('facebook_api')
+
+ACCESS_TOKEN = getattr(settings, 'FACEBOOK_API_ACCESS_TOKEN', None)
 
 def get_tokens():
     '''
@@ -23,19 +26,23 @@ def get_api():
     '''
     Return API instance with latest token from database
     '''
-    tokens = get_tokens()
-    if not tokens:
-        update_token()
+    if ACCESS_TOKEN:
+        token = ACCESS_TOKEN
+    else:
         tokens = get_tokens()
-    t = tokens[0]
-    return Graph(t.access_token)
+        if not tokens:
+            update_token()
+            tokens = get_tokens()
+        token = tokens[0].access_token
+    return Graph(token)
 
 def graph(method, **kwargs):
     '''
     Call API using access_token
     '''
+    api = get_api()
     try:
-        return get_api()[method](**kwargs)
+        response = api[method](**kwargs)
     except GraphException, e:
         if e.code == 190:
             update_token()
@@ -45,9 +52,16 @@ def graph(method, **kwargs):
             return graph(method, **kwargs)
         else:
             raise e
-    except JSONDecodeError, e:
-        log.error("JSONDecodeError error: %s registered while executing method %s with params %s" % (e, method, kwargs))
-        raise e
+    except ValueError, e:
+        log.warning("ValueError: %s registered while executing method %s with params %s" % (e, method, kwargs))
+        # sometimes returns this dictionary, sometimes empty response, covered by test "test_empty_result"
+        data = {"error_code":1,"error_msg":"An unknown error occurred"}
+        return None
     except Exception, e:
         log.error("Unhandled error: %s registered while executing method %s with params %s" % (e, method, kwargs))
         raise e
+
+    if response.error_code:
+        log.error("Error %s: %s returned while executing method %s with params %s" % (response.error_code, response.error_msg, method, kwargs))
+
+    return response
