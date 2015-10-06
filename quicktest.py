3a8b775ce7c357018603dd773d70297f2cc77285
ramusus <ramusus@gmail.com>
2015-10-06 21:00:34 +0300

    updated license, quicktest.py

--- quicktest.py
+++ quicktest.py
@@ -1,16 +1,36 @@
-import os
-import sys
-import argparse
-from django.conf import settings
+'''
+Copyright 2011-2015 ramusus
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 
 '''
 QuickDjangoTest module for testing in Travis CI https://travis-ci.org
-Changes log:
+Change log:
  * 2014-10-24 updated for compatibility with Django 1.7
  * 2014-11-03 different databases support: sqlite3, mysql, postgres
+ * 2014-12-31 pep8, python 3 compatibility
+ * 2015-10-06 apache 2 license
 '''
 
+import argparse
+import os
+import sys
+
+from django.conf import settings
+
+
 class QuickDjangoTest(object):
+
     """
     A quick way to run the Django test suite without a fully-configured project.
 
@@ -85,11 +105,9 @@ class QuickDjangoTest(object):
 
     def get_custom_settings(self):
         try:
-            from settings_test import *
-            settings_test = dict(locals())
-            del settings_test['self']
-            if 'INSTALLED_APPS' in settings_test:
-                del settings_test['INSTALLED_APPS']
+            import settings_test
+            settings_test = dict([(k, v) for k, v in settings_test.__dict__.items() if k[0] != '_'])
+            INSTALLED_APPS = settings_test.pop('INSTALLED_APPS', [])
         except ImportError:
             settings_test = {}
             INSTALLED_APPS = []
@@ -102,12 +120,12 @@ class QuickDjangoTest(object):
         """
         INSTALLED_APPS, settings_test = self.get_custom_settings()
 
-        settings.configure(DEBUG = True,
-            DATABASE_ENGINE = 'sqlite3',
-            DATABASE_NAME = os.path.join(self.DIRNAME, 'database.db'),
-            INSTALLED_APPS = self.INSTALLED_APPS + INSTALLED_APPS + self.apps,
-            **settings_test
-        )
+        settings.configure(DEBUG=True,
+                           DATABASE_ENGINE='sqlite3',
+                           DATABASE_NAME=os.path.join(self.DIRNAME, 'database.db'),
+                           INSTALLED_APPS=self.INSTALLED_APPS + INSTALLED_APPS + self.apps,
+                           **settings_test
+                           )
         from django.test.simple import run_tests
         failures = run_tests(self.apps, verbosity=1)
         if failures:
@@ -120,9 +138,9 @@ class QuickDjangoTest(object):
         INSTALLED_APPS, settings_test = self.get_custom_settings()
 
         settings.configure(
-            DEBUG = True,
-            DATABASES = self.get_database(),
-            INSTALLED_APPS = self.INSTALLED_APPS + INSTALLED_APPS + self.apps,
+            DEBUG=True,
+            DATABASES=self.get_database(),
+            INSTALLED_APPS=self.INSTALLED_APPS + INSTALLED_APPS + self.apps,
             **settings_test
         )
 
@@ -138,10 +156,10 @@ class QuickDjangoTest(object):
         INSTALLED_APPS, settings_test = self.get_custom_settings()
 
         settings.configure(
-            DEBUG = True,
-            DATABASES = self.get_database(),
-            MIDDLEWARE_CLASSES = ('django.middleware.common.CommonMiddleware',
-                                  'django.middleware.csrf.CsrfViewMiddleware'),
+            DEBUG=True,
+            DATABASES=self.get_database(),
+            MIDDLEWARE_CLASSES=('django.middleware.common.CommonMiddleware',
+                                'django.middleware.csrf.CsrfViewMiddleware'),
             INSTALLED_APPS = self.INSTALLED_APPS + INSTALLED_APPS + self.apps,
             **settings_test
         )
