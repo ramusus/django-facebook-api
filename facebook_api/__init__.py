9b32135ae3ded6ab6e1407182545f461aeb31573
ramusus <ramusus@gmail.com>
2016-04-03 20:12:42 +0300

    bump version

--- facebook_api/__init__.py
+++ facebook_api/__init__.py
@@ -12,5 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VERSION = (0, 6, 7)
+VERSION = (0, 6, 8)
 __version__ = '.'.join(map(str, VERSION))
--- facebook_api/parser.py
+++ facebook_api/parser.py
@@ -13,11 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-from datetime import datetime, timedelta
-import re
-
 from bs4 import BeautifulSoup
-from django.conf import settings
 from oauth_tokens.providers.facebook import FacebookAccessToken
 import requests
 
